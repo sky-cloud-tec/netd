26c8743395d73d1dd49226f3583703323e57b81f
songtianyi <songtianyi630@163.com>
2020-01-19 11:23:33 +0800

    fix: docker login

--- cd.sh
+++ cd.sh
@@ -5,11 +5,11 @@ TRAVIS_BRANCH=master
 if [ "$TRAVIS_BRANCH" != "master" ]
 then 
 	    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!" 
-		    exit 0
+		exit 1
 fi
 
 rev=$(git rev-parse --short HEAD)
-docker login -u docker-image-builder -p $HUB hub.sky-cloud.net
+echo "$HUB" | docker login -u docker-image-builder  --password-stdin
 docker build -t hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}_build-${rev} .
 docker push hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}_build-${rev}
 
