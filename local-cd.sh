237a94d34bcd3f0ea84b48ca3937d5e835651f27
songtianyi <songtianyi630@163.com>
2020-03-27 18:03:35 +0800

    add: local-cd file

--- local-cd.sh
+++ local-cd.sh
@@ -9,8 +9,8 @@ then
 fi
 
 rev=$(git rev-parse --short HEAD)
-sudo cat /etc/docker/daemon.json | jq '. + {"insecure-registries": ["hub.sky-cloud.net"]}' | sudo tee /etc/docker/daemon.json
-sudo service docker restart
-echo "$HUB" | docker login -u docker-image-builder  http://hub.sky-cloud.net --password-stdin
+docker login -u docker-image-builder  http://hub.sky-cloud.net
 docker build -t hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}_build-${rev} .
 docker push hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}_build-${rev}
+
+
