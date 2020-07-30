aafb12dde6cb8fb0db93144e00c4e148877b0fd3
songtianyi <songtianyi630@163.com>
2020-07-30 18:06:23 +0800

    refact: use debug level log for encoding stuff

--- cli/conn/conn.go
+++ cli/conn/conn.go
@@ -444,7 +444,7 @@ outside:
 					logs.Error(s.req.LogPrefix, "detect origin encoding error,", err)
 				} else {
 					// print origin encoding
-					logs.Info(s.req.LogPrefix, "detected encoding", dr, "predefined encoding", s.op.GetEncoding())
+					logs.Debug(s.req.LogPrefix, "detected encoding", dr, "predefined encoding", s.op.GetEncoding())
 				}
 				// convert, even if detect error
 				u8buf, err := common.ConvToUTF8(s.op.GetEncoding(), rbuf[:beginIdx])
@@ -452,10 +452,10 @@ outside:
 					logs.Error(s.req.LogPrefix, "conv to utf8 error", err)
 				} else {
 					// conv ok, compare size then log
-					logs.Info(s.req.LogPrefix, "origin size", len(rbuf[:beginIdx]), "converted size", len(u8buf))
+					logs.Debug(s.req.LogPrefix, "origin size", len(rbuf[:beginIdx]), "converted size", len(u8buf))
 					// dectect again
 					dr, err = d.DetectBest(u8buf)
-					logs.Info(s.req.LogPrefix, "detected encoding", dr, err)
+					logs.Debug(s.req.LogPrefix, "detected encoding", dr, err)
 				}
 				// use converted content
 				// don't worry, if not converted, original byte slice will be retured
