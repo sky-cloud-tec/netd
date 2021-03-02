3cc33bb74b8c09f9a1f564704b6a2e6d4f988ef8
skycloud-songtianyi <songtianyi@sky-cloud.net>
2021-03-02 18:19:48 +0800

    chore: change branch name

--- local-ci.sh
+++ local-ci.sh
@@ -2,18 +2,18 @@
 
 set -o errexit -o nounset
 CGO_ENABLE=0 GOOS=linux GOARCH=amd64 go build .
-TRAVIS_BRANCH=master
-if [ "$TRAVIS_BRANCH" != "master" ]
+TRAVIS_BRANCH=xyzq
+if [ "$TRAVIS_BRANCH" != "xyzq" ]
 then 
-	    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!" 
+	    echo "This commit was made against the $TRAVIS_BRANCH and not the xyzq! No deploy!" 
 		exit 1
 fi
 
 rev=$(git rev-parse --short HEAD)
-num=$(git rev-list --count master)
+num=$(git rev-list --count xyzq)
 name=hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}-local-${rev}-${num}
 docker login -u docker-image-builder  http://hub.sky-cloud.net
 docker build -t ${name} .
-docker push ${name}
+#docker push ${name}
 
 
