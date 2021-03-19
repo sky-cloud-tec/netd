1b439cec7812cfbec3349ef7af93040432b8df26
skycloud-songtianyi <songtianyi@sky-cloud.net>
2021-03-19 17:16:01 +0800

    update

--- local-ci.sh
+++ local-ci.sh
@@ -2,16 +2,16 @@
 
 set -o errexit -o nounset
 CGO_ENABLE=0 GOOS=linux GOARCH=amd64 go build .
-TRAVIS_BRANCH=master
-if [ "$TRAVIS_BRANCH" != "master" ]
+TRAVIS_BRANCH=feat_config_based_engine
+if [ "$TRAVIS_BRANCH" != "feat_config_based_engine" ]
 then 
 	    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!" 
 		exit 1
 fi
 
 rev=$(git rev-parse --short HEAD)
-num=$(git rev-list --count master)
-name=hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}_build-${rev}-${num}
+num=$(git rev-list --count feat_config_based_engine)
+name=hub.sky-cloud.net/nap2/netd:${TRAVIS_BRANCH}_local-${rev}-${num}
 docker login -u docker-image-builder  http://hub.sky-cloud.net
 docker build -t ${name} .
 docker push ${name}
