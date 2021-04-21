f30a3b8da3f6c2c88343550c5cf257328ac45ee6
skycloud-songtianyi <songtianyi@sky-cloud.net>
2021-04-21 16:44:10 +0800

    fix: ci

--- ci.sh
+++ ci.sh
@@ -1,13 +1,13 @@
 set -o errexit -o nounset
-TRAVIS_BRANCH=master
-if [ "$TRAVIS_BRANCH" != "master" ]
+TRAVIS_BRANCH=xyzq
+if [ "$TRAVIS_BRANCH" != "xyzq" ]
 then 
-	    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!" 
+	    echo "This commit was made against the $TRAVIS_BRANCH and not the xyzq! No deploy!" 
 		exit 1
 fi
 
 rev=$(git rev-parse --short HEAD)
-num=$(git rev-list --count master)
+num=$(git rev-list --count xyzq)
 name=registry.cn-hangzhou.aliyuncs.com/sky-cloud-tec/netd:${TRAVIS_BRANCH}-travis-${rev}-${num}
 sudo cat /etc/docker/daemon.json | jq '. + {"insecure-registries": ["registry.cn-hangzhou.aliyuncs.com"]}' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
